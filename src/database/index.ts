7b0d49c01aac4bca33e494624fea5daa9b78d954
minjman2659 <minjman2659@gmail.com>
2022-02-21 19:51:32 +0900

    Fix: Sequelize Warning 해결

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,43 +1,6 @@
-import { Sequelize, Op } from 'sequelize';
+import { Sequelize } from 'sequelize';
 
 const { POSTGRES_DATABASE, POSTGRES_USER, POSTGRES_PW } = process.env;
-
-// const operatorsAliases = {
-//   $eq: Op.eq,
-//   $ne: Op.ne,
-//   $gte: Op.gte,
-//   $gt: Op.gt,
-//   $lte: Op.lte,
-//   $lt: Op.lt,
-//   $not: Op.not,
-//   $in: Op.in,
-//   $notIn: Op.notIn,
-//   $is: Op.is,
-//   $like: Op.like,
-//   $notLike: Op.notLike,
-//   $iLike: Op.iLike,
-//   $notILike: Op.notILike,
-//   $regexp: Op.regexp,
-//   $notRegexp: Op.notRegexp,
-//   $iRegexp: Op.iRegexp,
-//   $notIRegexp: Op.notIRegexp,
-//   $between: Op.between,
-//   $notBetween: Op.notBetween,
-//   $overlap: Op.overlap,
-//   $contains: Op.contains,
-//   $contained: Op.contained,
-//   $adjacent: Op.adjacent,
-//   $strictLeft: Op.strictLeft,
-//   $strictRight: Op.strictRight,
-//   $noExtendRight: Op.noExtendRight,
-//   $noExtendLeft: Op.noExtendLeft,
-//   $and: Op.and,
-//   $or: Op.or,
-//   $any: Op.any,
-//   $all: Op.all,
-//   $values: Op.values,
-//   $col: Op.col,
-// };
 
 export const sequelize = new Sequelize(
   POSTGRES_DATABASE,
@@ -47,7 +10,13 @@ export const sequelize = new Sequelize(
     dialect: 'postgres',
     logging: false,
     port: 5432,
-    // operatorsAliases,
+    // https://sequelize.org/master/manual/connection-pool.html 참고
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
   },
 );
 
